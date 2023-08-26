4f49fbd8667a2d5ea60f8f55c32932bf070ddbe4
dewiz <churkin.it@gmail.com>
2023-08-26 23:04:04 +0300

    Added virtual scrolling to improve performance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { AppCardComponent } from './app-card/app-card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {AppCardComponent} from './app-card/app-card.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatListModule} from "@angular/material/list";
 import {MatButtonModule} from "@angular/material/button";
 import {FilterPipe} from "./pipes/filter.pipe";
 import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {ScrollingModule} from "@angular/cdk/scrolling";
 
 @NgModule({
   declarations: [
@@ -17,12 +19,15 @@ import {FormsModule} from "@angular/forms";
     AppCardComponent,
     BrowserAnimationsModule,
     BrowserModule,
-    MatListModule,
-    MatButtonModule,
     FormsModule,
-    FilterPipe
+    FilterPipe,
+    HttpClientModule,
+    MatButtonModule,
+    MatListModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
