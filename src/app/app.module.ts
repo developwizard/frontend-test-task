204554e9d1a160cf69f16f6d76b0af57010fd9dc
dewiz <churkin.it@gmail.com>
2023-08-26 20:34:13 +0300

    Added FilterPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppCardComponent } from './app-card/app-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatListModule} from "@angular/material/list";
 import {MatButtonModule} from "@angular/material/button";
+import {FilterPipe} from "./pipes/filter.pipe";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        AppCardComponent,
-        BrowserAnimationsModule,
-        BrowserModule,
-        MatListModule,
-        MatButtonModule
-    ],
+  imports: [
+    AppCardComponent,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatListModule,
+    MatButtonModule,
+    FormsModule,
+    FilterPipe
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/pipes/filter.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+  standalone: true
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, searchValue: string, filteredOnlySelected: boolean): any {
+    if (!searchValue) return value;
+    return filteredOnlySelected
+      ? value.filter((v: any) => v.selected && v.description.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
+      : value.filter((v: any) => v.description.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
+  }
+}
