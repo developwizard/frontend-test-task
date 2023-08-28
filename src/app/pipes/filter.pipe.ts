707e9bec707aab40692a121782e2d465b55a547f
dewiz <churkin.it@gmail.com>
2023-08-28 12:18:06 +0300

    Added filtration by title

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -8,7 +8,7 @@ export class FilterPipe implements PipeTransform {
   transform(value: any, searchValue: string, filteredOnlySelected: boolean): any {
     if (!searchValue) return value;
     return filteredOnlySelected
-      ? value.filter((v: any) => v.selected && v.description.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
-      : value.filter((v: any) => v.description.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
+      ? value.filter((v: any) => v.selected && (v.description.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 || v.title.toLowerCase().indexOf(searchValue.toLowerCase()) > -1))
+      : value.filter((v: any) => (v.description.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 || v.title.toLowerCase().indexOf(searchValue.toLowerCase()) > -1))
   }
 }
