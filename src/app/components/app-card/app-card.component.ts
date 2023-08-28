d50ee0fee94f4ef74a3544edc276b98277283ba3
dewiz <churkin.it@gmail.com>
2023-08-28 19:07:54 +0300

    Added icons svg

--- src/app/components/app-card/app-card.component.ts
+++ src/app/components/app-card/app-card.component.ts
@@ -26,18 +26,33 @@ export class AppCardComponent implements OnInit {
   @Input()
   blocksCount: number = 0;
   blockCountText: string = '';
+  blockIconSrc: string = '';
   @Input()
   selected: boolean = false;
   @Output() selectionChanges = new EventEmitter<boolean>();
 
   ngOnInit(): void {
-    this.setBlockCountText(this.blocksCount);
+    this.setBlockCountIconAndText(this.blocksCount);
   }
 
-  setBlockCountText(blockCount: number) {
-      this.blockCountText = blockCount == 1
-        ? blockCount + ' block'
-        : blockCount + ' blocks';
+  setBlockCountIconAndText(blockCount: number) {
+    this.blockCountText = blockCount == 1
+      ? blockCount + ' block'
+      : blockCount + ' blocks';
+    this.blockIconSrc = this.getBlockIconSrc(blockCount);
+  }
+
+  getBlockIconSrc(blockCount: number): string {
+    switch (blockCount) {
+      case 1:
+        return 'assets/svg/1.svg';
+      case 2:
+        return 'assets/svg/2.svg';
+      case 3:
+        return 'assets/svg/3.svg';
+      default:
+        return 'assets/svg/4.svg';
+    }
   }
 
   onSelectionChanges(selected: boolean) {
